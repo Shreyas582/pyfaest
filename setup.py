1cd7d9d9ed5f39587d5eed73998b01ab84a647f7
Shreyas Sankpal <shreyas.58@outlook.com>
2025-12-02 19:33:52 -0500

    Bump version to 1.0.7

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ all_packages = base_packages + lib_packages
 
 setup(
     name='pyfaest',
-    version='1.0.6',
+    version='1.0.7',
     author='PyFAEST Contributors',
     author_email='',
     maintainer='Shreyas Sankpal',
