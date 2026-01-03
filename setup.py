f55bd72f3473f966d62523e3b861400dbc04ee04
Shreyas Sankpal <shreyas.58@outlook.com>
2026-01-03 00:09:59 -0500

    Fix Windows wheel repair - use Python zipfile instead of Compress-Archive - v1.0.39

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ all_packages = base_packages + lib_packages
 
 setup(
     name='pyfaest',
-    version='1.0.38',
+    version='1.0.39',
     author='PyFAEST Contributors',
     author_email='',
     maintainer='Shreyas Sankpal',
